131c37933919f5bdf1428c41b7303ee10e24d55d
kubos777 <chd.jl@gmail.com>
2018-10-30 21:13:49 -0600

    :tired: Subiendo producto punto con openmp, verificar con clausula schedule

--- OpenMP/productoPunto.c
+++ OpenMP/productoPunto.c
@@ -3,35 +3,40 @@
 #include <stdlib.h>
 #include "omp.h"
 
+double prod_punto(double* x,double* y,int N);
 
+double prod_punto(double* x,double* y,int N){
+   //Producto punto
+  double dot,sum_local = 0.0;
+  #pragma omp parallalel for private(i) shared(sum_local) 
+  for(int i=0;i<N;i++){
+    #pragma omp critical
+    sum_local += x[i]*y[i];
+  }
+  printf("Inicio : 0, n : %d, sum_local : %.2f\n",N,sum_local);
+  
+  //printf("sum_local: %.2f, dot : %.2f\n",sum_local,dot);
+  dot+=sum_local;
+  
+  //printf("El producto punto es %.2f \n", dot);
+ return dot;
+}
 int main(int argc, char* argv[]) 
 {
-  double *x, *y, *local_x, *local_y;
-  int    res, n_local; 
-  double  dot, local_dot,sum_local;
-  int    inicio,size, rank, i, n=2;
-  
-  printf("Tamaño del vector %d\n",n);
-    // scanf("%d", &n);
+  double *x, *y,producto_punto;
+  int    res, n,i;
+  printf("Ingresa el tamaño del vector: \n");
+  scanf("%d", &n);
   x = (double *) calloc(n, sizeof(double));
   y = (double *) calloc(n, sizeof(double));
   //Llenado
   for (i=0; i<n; i++) 
   {
-    x[i] = 1.0;  y[i] = 1.0;
+    x[i] = 1.0;  y[i] = 3.0;
   }
-//Producto punto
-#pragma omp parallalel for private(i) shared(sum_local) 
-  for(i=inicio;i<n;i++){
-    #pragma omp critical
-    sum_local += x[i]*y[i];
-  }
-  printf("Inicio : %d, n : %d, sum_local : %.2f\n",inicio,n,sum_local);
-  
-  //printf("sum_local: %.2f, dot : %.2f\n",sum_local,dot);
-  dot+=sum_local;
-  
-  printf("El producto punto es %.2f \n", dot);
+ producto_punto = prod_punto(x,y,n);
+printf("El producto punto es: %f\n",producto_punto);  
+return 0;
 
-  return 0;
-}  
+
+}
