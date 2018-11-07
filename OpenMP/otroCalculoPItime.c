042f484e426186866cb7511fc1209c509465accd
kubos777 <chd.jl@hotmail.com>
2018-11-06 18:19:55 -0600

    Agrego calculo de PI con medición de tiempo

--- OpenMP/otroCalculoPItime.c
+++ OpenMP/otroCalculoPItime.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <omp.h>
+#include <time.h>
+
 static long num_steps = 100000;double step;
 #define NUM_THREADS 2
 
 void main(){
-	double empezar,terminar;
+	double tiempo_total;
+ 	clock_t inicio, fin;
 	int i;double x,pi,sum = 0.0;
 	step = 1.0/(double) num_steps;
 	omp_set_num_threads(NUM_THREADS);
-	empezar = omp_get_wtime();
+	
+	inicio = clock(); //Inicia el conteo
+	
 	#pragma omp parallel for reduction(+:sum) private(x)
 	for(i=1;i<=num_steps;i++){
 		x = (i-0.5)*step;
 		sum = sum + 4.0/(1.0+x*x);
 	}
-	terminar = omp_get_wtime();
-	printf("Empezar: %1f [milisegundos]\n", empezar );
-	printf("Terminar: %1f [milisegundos]\n",terminar );
-	printf("Tiempo %1f [milisegundos]\n",terminar-empezar );
+	
+	fin = clock(); //Termina medición
+	
+	printf("Tiempo total %1f [milisegundos]\n",(double)(fin-inicio)/1000 );
 	pi = step*sum;
 }
-
